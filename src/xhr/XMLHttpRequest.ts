68e3263456d13a23ae8154bb6ed5e2d7e1ba372f
liaoqinwei <2573424017@qq.com>
2020-12-10 21:54:07 +0800

    ''

--- src/xhr/XMLHttpRequest.ts
+++ src/xhr/XMLHttpRequest.ts
@@ -1,6 +1,6 @@
 const http = require('http')
-const https = require('https')
 const fs = require('fs')
+const url = require('url')
 
 type method = 'get' | 'post' | 'put' | 'options' | 'delete' | 'patch' | 'connect' | 'trace' | 'head'
 
@@ -9,59 +9,95 @@ let netPathReg = /^http[s]?/
 // node 环境下没有 XHR 我们需要封装一个
 // @ts-ignore
 class XMLHttpRequest {
-  readonly DONE = 4
-  readonly HEADERS_RECEIVED = 2
-  readonly LOADING = 3
-  readonly OPENED = 1
-  readonly UNSENT = 0
+    url: string
+    method: method
+    readonly DONE = 4
+    readonly HEADERS_RECEIVED = 2
+    readonly LOADING = 3
+    readonly OPENED = 1
+    readonly UNSENT = 0
+    headers = {}
+    timeout = 1000 * 10
 
-  timeout
+    // response
+    readonly response
+    readonly responseText
+    responseType
 
-  // response
-  readonly response
-  readonly responseText
-  responseType
+    // state
+    statusText
+    status
+    readyState = this.UNSENT
 
-  // state
-  statusText
-  status
-  readyState = this.UNSENT
-
-  // event
-  onabort
-  onerror
-  ontimeout
-  onreadystatechange
+    // event
+    onabort
+    onerror
+    ontimeout
+    onreadystatechange
 
 
-  // 不支持同步请求
-  open(method: method, url: string): void {
-    this.readyState = this.OPENED
-  }
+    // 不支持同步请求
+    open(method: method, url: string): void {
+        this.readyState = this.OPENED
+        this.url = url
+        this.method = method
+    }
 
-  send(data: object): void {
-    this.readyState = this.HEADERS_RECEIVED
-    // 发送请求
-    this.readyState = this.LOADING
-    // 请求完成
-    this.readyState = this.DONE
-  }
+    /*
+    * url 的情况
+    * 文件
+    * /sdad/sadsad
+    * file:///E:\sadad\sad
+    *
+    * http://
+    * https://
+    *
+    *
+    * */
+    send(data: object): void {
+        // send
+        this.readyState = this.HEADERS_RECEIVED
+        let options = {...url.parse(this.url), method: this.method, headers: this.headers,}
+        let protocol = options.protocol
+        if (protocol !== null) { // http / https
+          // https for 443 port
+          if(protocol === 'https:')
+            options.port = 443
+          http.request(options,(res)=>{
+            console.log(res)
+          }).end()
+        } else { // file
 
-  abort(): void {
-  }
+        }
+        // 发送请求
+        this.readyState = this.LOADING
+        // 请求完成
+        this.readyState = this.DONE
+    }
 
-  getResponseHeader(header: string) {
-  }
+    abort(): void {
+    }
 
-  getAllResponseHeaders(): string {
-    return ''
-  }
+    getResponseHeader(header: string) {
+    }
 
-  setRequestHeader(header: string, value: string) {
-    if (this.readyState !== this.OPENED) return
-  }
+    getAllResponseHeaders(): string {
+        return ''
+    }
 
-  // 此方法暂定
-  overrideMimeType() {
-  }
+    setRequestHeader(header: string, value: string) {
+        if (this.readyState !== this.OPENED) return
+        this.headers[header] = value
+    }
+
+    // 此方法暂定
+    overrideMimeType() {
+    }
 }
+
+
+// test
+let xhr = new XMLHttpRequest()
+xhr.open('get','http://152.136.147.123:9090/recommend')
+xhr.send(null)
+console.log(123)
---
+++ src/xhr/request.ts
@@ -0,0 +1,6 @@
+const http = require('http')
+const https = require('https')
+const fs = require('fs')
+
+
+function request(options){}
